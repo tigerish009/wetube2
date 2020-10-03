bdb7d82e7003e1b981ad70fb8798e6062daf392c
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-10-03 16:23:33 +0900

    middleware_start201003

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
 
 const PORT = 4000;
@@ -8,6 +9,13 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) => res.send("Hi from Home!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("Between");
+    next();
+}
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
