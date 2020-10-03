c1a21288a10d6ff68f7f9d1e13052ad8055aa3de
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-10-03 15:52:25 +0900

    babel&nodemon201003

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res) {
-    console.log(req);
-    res.send("Hi from Home!!");
-}
-function handleProfile(req, res) {
-    res.send("You are on my profile");
-}
+const handleHome = (req, res) => res.send("Hi from Home!!");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
