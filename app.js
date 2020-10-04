0c95873940ef9e437effeb846250c5e35f1d1bbf
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-10-04 16:36:41 +0900

    app_export&init.js201004

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-
 const handleHome = (req, res) => res.send("Hi from Home!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
@@ -24,5 +20,4 @@ app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-
-app.listen(PORT, handleListening);
+export default app;
