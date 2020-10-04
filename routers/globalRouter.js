9a1aa5871e50031c01ecd150d45def2f717820ac
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-10-04 18:32:16 +0900

    controllers&routers201004

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
+import { join, login, logout } from "../controllers/userControllers";
+import { home, search } from "../controllers/videoControllers";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send("Home"));
-globalRouter.get(routes.join, (req, res) => res.send("Join"));
-globalRouter.get(routes.login, (req, res) => res.send("Logout"));
-globalRouter.get(routes.logout, (req, res) => res.send("Logout"));
-globalRouter.get(routes.search, (req, res) => res.send("Search"));
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
