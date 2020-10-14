ccb5d197f1b61e603668741528623fbbe9514168
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-10-14 22:01:49 +0900

    async&await_try&catch_Video.find201014

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,7 +1,24 @@
-import routes from "../routes"
+import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    res.render("home", {pageTitle : "Home", videos}); //pug 파일명인식
+// async는 멸령을 마칠때까지 기다려서 타이밍 맞춰줌
+// 자바스크립트는 여러명령을 동시에 진행하고, 실행후 끝나지 않아도 다음으로 넘어감
+// 그래서, 정의한 코드를 실행않고 다음으로 넘어가서 값이 없는것처럼 나옴
+// 단, 에러발생시켜도 다음으로 넘어감. 정확한지는 판단안하고 명령이 끝난지만 봄
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({}); 
+    // await는 명령이 끝날때까지 기다리란 의미 (await는 async와 세트)
+    // Video.find({}) 하면 모든 비디오 가져옴
+        res.render("home", {pageTitle : "Home", videos}); //pug 파일명인식
+    } catch(error) {
+        console.log(error);
+        res.render("home", { pateTitle: "home", videos: []});
+        // 에러발생하면 비디오에 빈 배열생성
+    }
+    // try & catch 는 에러판단없이 진행될때 알아채기 위해서 사용함
+    // try는 에러없을때 시도할 내용
+    // catch에서 에러발생시 콘솔표기해줌
 }
 
 export const search = (req, res) => {
