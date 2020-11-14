a48135bd103b7ca4699f4c055fc3889b5f40813a
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-11-14 20:56:35 +0900

    Join_User.register201114

--- passport.js
+++ passport.js
@@ -2,3 +2,9 @@ import passport from "passport";
 import User from "./models/User";
 
 passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser()); //쿠키에 id를 담고
+passport.deserializeUser(User.deserializeUser()); //어떤 id인지 알아낸다
+// 대부분이 똑같이 사용하는 패턴임
+
+
